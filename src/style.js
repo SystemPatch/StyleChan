d24d27548ad312edc0204f3783ff82a869b819c8
3nly <59023753+3nly@users.noreply.github.com>
2024-11-29 15:17:44 +0000

    Themes custom CSS wasn't being parsed

--- src/style.js
+++ src/style.js
@@ -3,10 +3,10 @@
 <%= grunt.file.read('src/css/General.css') %>
 <%= grunt.file.read('src/css/Colors.css') %>
 <%= grunt.file.read('src/css/Fonts.css') %>
+" + $SS.theme.customCSS + "
 " + ($SS.conf["Sidebar Position"] !== 3 ? "<%= grunt.file.read('src/css/Sidebar.css') %>" : "") + "
 <%= grunt.file.read('src/css/Quickreply.css') %>
 <%= grunt.file.read('src/css/Icons.css') %>
 <%= grunt.file.read('src/css/Highlight.css') %>
 <%= grunt.file.read('src/css/Options.css') %>
 <%= grunt.file.read('src/css/Catalog.css') %>
-" + $SS.theme.customCSS + "
