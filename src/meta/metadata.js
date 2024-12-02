d3a32064f8775c0cbe896110d415bd619fb946c1
3nly <59023753+3nly@users.noreply.github.com>
2024-12-02 02:38:42 +0000

    Meta stuff

--- src/meta/metadata.js
+++ src/meta/metadata.js
@@ -7,9 +7,32 @@
 // @minFFVer     <%= meta.min.firefox %>
 // @license      <%= license %>; <%= meta.repo %>blob/<%= meta.mainBranch %>/LICENSE 
 <%=
-meta.matches.map(function(match) {
-return '// @match        ' + match;
-}).join('\n')
+  (function() {
+    function expand(items, regex, substitutions) {
+      var results = [];
+      items.forEach(function(item) {
+        if (regex.test(item)) {
+          substitutions.forEach(function(s) {
+            results.push(item.replace(regex, s));
+          });
+        } else {
+          results.push(item);
+        }
+      });
+      return results;
+    }
+    function expandMatches(matches) {
+      return expand(matches, /^\*/, ['http', 'https']);
+    }
+    return [].concat(
+      expandMatches(meta.matches).map(function(match) {
+        return '// @match        ' + match;
+      }),
+      expandMatches(meta.exclude_matches).map(function(match) {
+        return '// @exclude      ' + match;
+      })
+    ).join('\n');
+  })()
 %>
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -21,40 +44,6 @@ return '// @match        ' + match;
 // @grant        GM.deleteValue
 // @grant        GM.listValues
 // @grant        GM.openInTab
-// @include      http://boards.4chan.org/*
-// @include      https://boards.4chan.org/*
-// @include      http://sys.4chan.org/*
-// @include      https://sys.4chan.org/*
-// @include      http://boards.4channel.org/*
-// @include      https://boards.4channel.org/*
-// @include      http://sys.4channel.org/*
-// @include      https://sys.4channel.org/*
-// @exclude      http://www.4channel.org/*
-// @exclude      https://www.4channel.org/*
-// @exclude      http://www.4chan.org/pass
-// @exclude      https://www.4chan.org/pass
-// @exclude      http://www.4chan.org/pass?*
-// @exclude      https://www.4chan.org/pass?*
-// @exclude      http://www.4chan.org/advertise
-// @exclude      https://www.4chan.org/advertise
-// @exclude      http://www.4chan.org/advertise?*
-// @exclude      https://www.4chan.org/advertise?*
-// @exclude      http://www.4chan.org/donate
-// @exclude      https://www.4chan.org/donate
-// @exclude      http://www.4chan.org/donate?*
-// @exclude      https://www.4chan.org/donate?*
-// @exclude      http://www.4channel.org/pass
-// @exclude      https://www.4channel.org/pass
-// @exclude      http://www.4channel.org/pass?*
-// @exclude      https://www.4channel.org/pass?*
-// @exclude      http://www.4channel.org/advertise
-// @exclude      https://www.4channel.org/advertise
-// @exclude      http://www.4channel.org/advertise?*
-// @exclude      https://www.4channel.org/advertise?*
-// @exclude      http://www.4channel.org/donate
-// @exclude      https://www.4channel.org/donate
-// @exclude      http://www.4channel.org/donate?*
-// @exclude      https://www.4channel.org/donate?*
 // @run-at       document-start
 // @updateURL    <%= meta.downloads %><%= meta.files.metajs %>
 // @downloadURL  <%= meta.downloads %><%= meta.files.userjs %>
